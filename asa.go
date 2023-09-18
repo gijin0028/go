29376271c0563b35cac300220a8f292920e07221
gijin0028 <111556051+gijin0028@users.noreply.github.com>
2023-09-18 21:30:11 +0900

    Add files via upload

---
+++ asa.go
@@ -0,0 +1,17 @@
+package main
+
+import "fmt"
+
+var name string = "김재환"
+var a int = 32
+var b int = 65
+
+func main() {
+
+	fmt.Println(a + b)
+	fmt.Println(a)
+	fmt.Println(b)
+	fmt.Println("hello")
+	fmt.Println("나의 이름은", name, "이다")
+
+}
